fd835943bc962f5e7f552cc8e76082b3e30252aa
RJ <90836237+HR-RJ@users.noreply.github.com>
2022-11-29 15:49:51 +0100

    added rgb to hsl and start hsl to rgb

--- Week2/color2.cpp
+++ Week2/color2.cpp
@@ -4,41 +4,67 @@
 
 #include <iostream>
 #include <string>
-#include <vector>
-#include <algorithm>
 
-// template <typename T, size_t N> const T* mybegin(const T (&a)[N]) { return a; }
-// template <typename T, size_t N> const T* myend  (const T (&a)[N]) { return a+N; }
-
-// using namespace std;
-float r,g,b,c,m,y,h,s,l;
 using namespace std;
+float r,g,b,c,m,y,h,s,l;
+float chroma;
+float hue;
+
 float *RGBtoCMY(float r, float g, float b) {
     // to do
-    return new float[3] {c = 1 - r, m = 1 - g, y = 1 -g };
+    return new float[3] {c = 1-r , m = 1-g, y=1-b};
 }
 
 float *CMYtoRGB(float c, float m, float y) {
     // to do
-
-    return new float[3] {r = 1 - c, g = 1 - m, b = 1 - y};
+    return new float[3] {r = 1-c, g = 1-m, b = 1-y};
 }
 
 float *RGBtoHSL(float r, float g, float b) {
     // to do
-    r /= 255;
-    g /= 255;
-    b /= 255;
-    float M[3]={r,g,b};
-    // auto max = *max_element(mybegin(M), myend(M));
-    // auto min = *min_element(mybegin(M), myend(M));
-    // float C = max - min;
-    // cout <<max << endl;
+    float Max = max(max(r,g),b);
+    float Min = min(min(r,g),b);
+    chroma = Max - Min;
+    float h1 = (r-g) / chroma + 4;
+    h = h1 * 60; //calculate h
+    l = (Min + Max) / 2;
+    if (l <= 0.5){
+        s = (Max - Min)/(Max + Min);
+    } else {
+        s = (Max - Min)/(2.0 - Max - Min);
+    }
     return new float[3] {h, s, l};
 }
 
 float *HSLtoRGB(float h, float s, float l) {
     // to do
+    float templ;
+    float templ2;
+    float hue;
+    float tempR, tempG, tempB;
+    if (l < 0.5){
+        float templ = l * (1.0*s);
+    } else {
+        float templ = l + s - l * s;
+    }
+    templ2 = 2 * l - templ;
+    hue = h / 360;
+    tempR = h + 0.333;
+    tempG = h;
+    tempB = h - 0.333;
+    if (6*tempR <1.0){
+        r = templ2 + (templ - templ2) * 6 *tempR;
+    } else if (2*tempR <=1.0)
+    {
+        r = templ;
+    } else if (3*tempR <2.0)
+    {
+        r = templ2 + (templ - templ2) * (0.666 - tempR) * 6;
+    } else {
+        r = templ2;
+    }
+    
+    
     return new float[3] {r, g, b};
 }
 
@@ -48,7 +74,7 @@ float *transparency(float r1, float g1, float b1, float alpha, float r2, float g
 }
 
 string to_string(float *f) {
-    return "(" + to_string(&f[0]) + ", " + to_string(&f[1]) + ", " + to_string(&f[2]) + ")"; 
+    return "(" + to_string(f[0]) + ", " + to_string(f[1]) + ", " + to_string(f[2]) + ")"; 
 }
 
 int main() {
