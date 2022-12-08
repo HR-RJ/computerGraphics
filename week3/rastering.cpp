7a9ebfab6d120dc51ebece056786254b848e02f4
RJ <90836237+HR-RJ@users.noreply.github.com>
2022-12-08 20:04:40 +0100

    finished rastering.cpp

--- week3/rastering.cpp
+++ week3/rastering.cpp
@@ -11,65 +11,76 @@
 #define GL_SILENCE_DEPRECATION
 // #include <GL/gl.h>
 #ifdef __APPLE__
-    #include <GLUT/glut.h>
+#include <GLUT/glut.h>
 #else
-    #include <GL/glut.h>
+#include <GL/glut.h>
 #endif
 
 #define GRID_SIZE 10
 
 using namespace std;
 
-class Point {
-    public:
-        int x, y;
-        Point(int, int);
+class Point
+{
+public:
+    int x, y;
+    Point(int, int);
 };
 
-Point::Point(int x, int y) {
+Point::Point(int x, int y)
+{
     this->x = x;
     this->y = y;
 }
 
-class Grid {
+class Grid
+{
     int sizeX, sizeY;
     list<Point> points;
-    public:
-        Grid(int, int);
-        void addPoint(int, int);
-        void rasterline(int, int, int, int);
-        void draw();
-        void display();
+
+public:
+    Grid(int, int);
+    void addPoint(int, int);
+    void rasterline(int, int, int, int);
+    void draw();
+    void display();
 };
 
 Grid *currentInstance;
 
-void display() {
+void display()
+{
     currentInstance->display();
 }
 
-void end(unsigned char key, int x, int y) {
+void end(unsigned char key, int x, int y)
+{
     exit(0);
 }
 
-Grid::Grid(int x, int y) {
+Grid::Grid(int x, int y)
+{
     sizeX = x;
     sizeY = y;
 }
 
-void Grid::display() {
+void Grid::display()
+{
     glClear(GL_COLOR_BUFFER_BIT);
     glColor3f(1, 1, 1);
-    for (auto &point: points) {
+    for (auto &point : points)
+    {
         glRecti(point.x * GRID_SIZE, point.y * GRID_SIZE, (point.x + 1) * GRID_SIZE, (point.y + 1) * GRID_SIZE);
     }
     glColor3f(.5, .5, .5);
     glBegin(GL_LINES);
-    for (int i = 1; i < sizeX; i++) {
+    for (int i = 1; i < sizeX; i++)
+    {
         glVertex2f(i * GRID_SIZE, 0);
         glVertex2f(i * GRID_SIZE, sizeY * GRID_SIZE);
     }
-    for (int i = 1; i < sizeY; i++) {
+    for (int i = 1; i < sizeY; i++)
+    {
         glVertex2f(0, i * GRID_SIZE);
         glVertex2f(sizeX * GRID_SIZE, i * GRID_SIZE);
     }
@@ -77,7 +88,8 @@ void Grid::display() {
     glFlush();
 }
 
-void Grid::draw() {
+void Grid::draw()
+{
     int argc = 0;
     glutInit(&argc, NULL);
     glutInitDisplayMode(GLUT_RGBA);
@@ -91,23 +103,55 @@ void Grid::draw() {
     glutMainLoop();
 }
 
-void Grid::addPoint(int x, int y) {
-    if (x < sizeX && y < sizeY && x >= 0 && y >= 0) {
+void Grid::addPoint(int x, int y)
+{
+    if (x < sizeX && y < sizeY && x >= 0 && y >= 0)
+    {
         points.emplace_back(x, y);
     }
 }
 
-void Grid::rasterline(int x0, int y0, int x1, int y1) {
-    addPoint(x0,y0);
-    addPoint(x1,y1);
-}
+void Grid::rasterline(int x0, int y0, int x1, int y1)
+{
+    //calculates the delta x and y
+    int dx = abs(x1 - x0);
+    int dy = abs(y1 - y0);
+    int sx = (x0 < x1) ? 1 : -1; //calculates the steps on the x-axis
+    int sy = (y0 < y1) ? 1 : -1; //calculatse the steps on the y-axis
+    int err = dx - dy; //calculates the error in the line to find the closest pixel that the line passes through
 
-int main() {
+    while (true) {
+        addPoint(x0, y0);
+
+        //stops drawing the line if the points are the same
+        if (x0 == x1 && y0 == y1) {
+            break;
+        }
+
+        int e2 = 2 * err;
+        if (e2 > -dy) {
+            err -= dy;//if the line is closer to the current pixel error is decremented
+            x0 += sx;
+        }
+        if (e2 < dx) {
+            err += dx;//if the line is closer to the next pixel error is incremented
+            y0 += sy;
+        }
+    }
+
+    addPoint(x0, y0);
+    addPoint(x1, y1);
+}
+int main()
+{
     // testcode
     // let op: de beoordeling wordt gedaan op basis van andere waarden
     Grid grid(20, 20);
     grid.rasterline(0, 0, 19, 19);
     grid.rasterline(0, 10, 19, 0);
+    grid.rasterline(0, 19, 19, 0);
+    grid.rasterline(10, 0, 10, 19);
+    grid.rasterline(0, 10, 19, 10);
     grid.draw();
     return 0; // never reached
 }
---
+++ week4/projection.cpp
@@ -0,0 +1,99 @@
+// Compileren:
+//
+// Op MacOS:
+// c++ -std=c++11 -o projection -framework OpenGL -framework GLUT projection.cpp
+//
+// Op Linux en Windows:
+// g++ -std=c++11 -o projection projection.cpp -l GL -l glut
+
+#include <list>
+
+#define GL_SILENCE_DEPRECATION
+// #include <GL/gl.h>
+#ifdef __APPLE__
+    #include <GLUT/glut.h>
+#else
+    #include <GL/glut.h>
+#endif
+
+using namespace std;
+
+class Line {
+    public:
+        int x1, y1, x2, y2;
+        Line(int, int, int, int);
+};
+
+Line::Line(int x1, int y1, int x2, int y2) {
+    this->x1 = x1;
+    this->y1 = y1;
+    this->x2 = x2;
+    this->y2 = y2;
+}
+
+class Lines {
+    int sizeX, sizeY;
+    list<Line> lines;
+    public:
+        Lines(int, int);
+        void addLine(int, int, int, int);
+        void draw();
+        void display();
+};
+
+Lines *currentInstance;
+
+void display() {
+    currentInstance->display();
+}
+
+void end(unsigned char key, int x, int y) {
+    exit(0);
+}
+
+Lines::Lines(int x, int y) {
+    sizeX = x;
+    sizeY = y;
+}
+
+void Lines::display() {
+    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(1, 1, 1);
+    glBegin(GL_LINES);
+    for (auto &line: lines) {
+        glVertex2i(line.x1, line.y1);
+        glVertex2i(line.x2, line.y2);
+    }
+    glEnd();
+    glFlush();
+}
+
+void Lines::draw() {
+    int argc = 0;
+    glutInit(&argc, NULL);
+    glutInitDisplayMode(GLUT_RGBA);
+    glutInitWindowSize(sizeX, sizeY);
+    glutCreateWindow("Lines");
+    glOrtho(0, sizeX, sizeY, 0, -1, 1);
+    currentInstance = this;
+    glutDisplayFunc(::display); // Cannot use instance function here, so must use function in global namespace
+    glutKeyboardFunc(end);
+    glutMainLoop();
+}
+
+void Lines::addLine(int x1, int y1, int x2, int y2) {
+        if (x1 < sizeX && y1 < sizeY && x1 >= 0 && y1 >= 0 && x2 < sizeX && y2 < sizeY && x2 >= 0 && y2 >= 0) {
+        lines.emplace_back(x1, y1, x2, y2);
+    }
+}
+
+int main() {
+    // testcode
+    Lines lines(640, 480);
+    lines.addLine(100, 100, 500, 300);
+    for (int i = 100; i < 600; i += 50) {
+        lines.addLine(100, 200, i, 400);
+    }
+    lines.draw();
+    return 0; // never reached
+}
